0e5328d9684ef98b1fe349ac9be6008acddcf517
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-19 22:10:58 -0500

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,30 @@
-// routes/index.js
-
 const express = require('express');
-const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+const Bull = require('bull');
 
-// Define routes and link them to controller methods
+const fileQueue = new Bull('fileQueue');
+
+const router = express.Router({ mergeParams: true });
+
+// User routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-
-// Add the new authentication routes
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);  // Assuming the UserController already exists
+router.get('/users/me', UsersController.getMe);
+//console.log('before call FilesController.getShow');
 
-module.exports = router;
+//File routes
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.post('/files', FilesController.postUpload);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = { router, fileQueue } ;
 
