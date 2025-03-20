e27e8ce766da5129b69a4637d6b2405069bb79ee
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-19 21:58:00 -0500

    updated user module

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
