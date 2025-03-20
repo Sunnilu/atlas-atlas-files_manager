b273b438c7cd29ee32a4e40ea1c4d1a5b5ac5ffd
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-19 22:03:31 -0500

    added 3 new endpoints

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 // Define routes and link them to controller methods
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-// Add the POST /users route
 router.post('/users', UsersController.postNew);
 
+// Add the new authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);  // Assuming the UserController already exists
+
 module.exports = router;
+
