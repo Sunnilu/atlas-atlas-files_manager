5964a13b96a4ed1d1092ff66e5a655fbdf27ec4c
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-19 21:52:09 -0500

    add users

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 // Define routes and link them to controller methods
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// Add the POST /users route
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
